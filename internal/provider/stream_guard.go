b133cacb48a9aab32f747f182fdef6ac328397bb
original.flipster <flipster.kehrer@gmail.com>
2026-06-19 22:59:36 +0200

    simplify idle guard

--- internal/provider/stream_guard.go
+++ internal/provider/stream_guard.go
@@ -11,51 +11,24 @@ const streamIdleTimeout = 30 * time.Second
 var errStreamStalled = errors.New("stream stalled: no data received within timeout")
 
 type stallGuard struct {
-	reset chan struct{}
-	stop  chan struct{}
+	timer *time.Timer
+	idle  time.Duration
 }
 
 func newStallGuard(ctx context.Context, idle time.Duration) (context.Context, *stallGuard) {
 	cctx, cancel := context.WithCancelCause(ctx)
-	g := &stallGuard{
-		reset: make(chan struct{}, 1),
-		stop:  make(chan struct{}),
-	}
-	go func() {
-		timer := time.NewTimer(idle)
-		defer timer.Stop()
-		for {
-			select {
-			case <-g.stop:
-				return
-			case <-cctx.Done():
-				return
-			case <-g.reset:
-				if !timer.Stop() {
-					select {
-					case <-timer.C:
-					default:
-					}
-				}
-				timer.Reset(idle)
-			case <-timer.C:
-				cancel(errStreamStalled)
-				return
-			}
-		}
-	}()
+	g := &stallGuard{idle: idle}
+	g.timer = time.AfterFunc(idle, func() { cancel(errStreamStalled) })
+	context.AfterFunc(cctx, func() { g.timer.Stop() })
 	return cctx, g
 }
 
 func (g *stallGuard) progress() {
-	select {
-	case g.reset <- struct{}{}:
-	default:
-	}
+	g.timer.Reset(g.idle)
 }
 
 func (g *stallGuard) done() {
-	close(g.stop)
+	g.timer.Stop()
 }
 
 func streamErr(streamCtx context.Context, scanErr error) error {
